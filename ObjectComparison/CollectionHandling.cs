e52462b3a6efa5653400d486aa29e60a6ccfb941
David <David@c0degeek.dev>
2024-10-29 16:06:55 +0100

    Refactor collection and numeric handling with detailed error handling
    Enhanced the `CollectionHandling` and `NumericComparison` classes by adding detailed error checks and exceptions. Improved handling of collections like dictionaries, sets, queues, and stacks, and introduced null argument checks to ensure robustness. Additionally, implemented a new method for comparing decimals in `NumericComparison`.

--- ObjectComparison/CollectionHandling.cs
+++ ObjectComparison/CollectionHandling.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Concurrent;
 
 namespace ObjectComparison;
 
@@ -7,183 +8,235 @@ namespace ObjectComparison;
 /// </summary>
 internal static class CollectionHandling
 {
-    public static object CloneCollection(Type collectionType, IEnumerable source,
-        Func<object, object> elementCloner)
+    private static readonly ConcurrentDictionary<Type, Type> ElementTypeCache = new();
+
+    public static object CloneCollection(Type collectionType, IEnumerable source, Func<object, object> elementCloner)
     {
-        // Handle arrays
-        if (collectionType.IsArray)
-        {
-            return CloneArray(collectionType, source, elementCloner);
-        }
+        ArgumentNullException.ThrowIfNull(collectionType);
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(elementCloner);
 
-        // Handle dictionaries
-        if (IsDictionary(collectionType))
-        {
-            return CloneDictionary(collectionType, source, elementCloner);
-        }
-
-        // Handle sets
-        if (IsSet(collectionType))
-        {
-            return CloneSet(collectionType, source, elementCloner);
-        }
-
-        // Handle queues and stacks
-        if (IsQueueOrStack(collectionType))
-        {
-            return CloneQueueOrStack(collectionType, source, elementCloner);
-        }
-
-        // Default to List<T> for other collection types
-        return CloneGenericList(collectionType, source, elementCloner);
+        return collectionType.IsArray ? CloneArray(collectionType, source, elementCloner)
+            : IsDictionary(collectionType) ? CloneDictionary(collectionType, source, elementCloner)
+            : IsSet(collectionType) ? CloneSet(collectionType, source, elementCloner)
+            : IsQueueOrStack(collectionType) ? CloneQueueOrStack(collectionType, source, elementCloner)
+            : CloneGenericList(collectionType, source, elementCloner);
     }
 
-    private static object CloneArray(Type arrayType, IEnumerable source,
-        Func<object, object> elementCloner)
+    private static object CloneArray(Type arrayType, IEnumerable source, Func<object, object> elementCloner)
     {
-        var elementType = arrayType.GetElementType();
+        var elementType = arrayType.GetElementType() ?? 
+            throw new ArgumentException($"Could not get element type for array type {arrayType.Name}");
+            
         var sourceArray = source.Cast<object>().ToArray();
         var array = Array.CreateInstance(elementType, sourceArray.Length);
 
-        for (var i = 0; i < sourceArray.Length; i++)
+        try
         {
-            array.SetValue(elementCloner(sourceArray[i]), i);
+            for (var i = 0; i < sourceArray.Length; i++)
+            {
+                var clonedElement = elementCloner(sourceArray[i]);
+                if (clonedElement is null && !elementType.IsClass)
+                {
+                    throw new InvalidOperationException($"Cannot assign null to array element of type {elementType.Name}");
+                }
+                array.SetValue(clonedElement, i);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new ComparisonException($"Failed to clone array of type {arrayType.Name}", "", ex);
         }
 
         return array;
     }
 
-    private static object CloneDictionary(Type dictType, IEnumerable source,
-        Func<object, object> elementCloner)
+    private static object CloneDictionary(Type dictType, IEnumerable source, Func<object, object> elementCloner)
     {
         var genericArgs = dictType.GetGenericArguments();
+        if (genericArgs.Length != 2)
+        {
+            throw new ArgumentException($"Dictionary type {dictType.Name} must have exactly two generic arguments");
+        }
+
         var keyType = genericArgs[0];
         var valueType = genericArgs[1];
-
         var dictType1 = typeof(Dictionary<,>).MakeGenericType(keyType, valueType);
-        var dict = Activator.CreateInstance(dictType1);
-        var addMethod = dictType1.GetMethod("Add");
+        var dict = Activator.CreateInstance(dictType1) ?? 
+            throw new InvalidOperationException($"Failed to create dictionary of type {dictType1.Name}");
+            
+        var addMethod = dictType1.GetMethod("Add") ?? 
+            throw new InvalidOperationException($"Could not find Add method on dictionary type {dictType1.Name}");
 
-        foreach (dynamic entry in source)
+        try
         {
-            var clonedKey = elementCloner(entry.Key);
-            var clonedValue = elementCloner(entry.Value);
-            addMethod.Invoke(dict, [clonedKey, clonedValue]);
+            foreach (dynamic entry in source)
+            {
+                var clonedKey = elementCloner(entry.Key);
+                var clonedValue = elementCloner(entry.Value);
+                addMethod.Invoke(dict, [clonedKey, clonedValue]);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new ComparisonException($"Failed to clone dictionary of type {dictType.Name}", "", ex);
         }
 
         return dict;
     }
 
-    private static object CloneSet(Type setType, IEnumerable source,
-        Func<object, object> elementCloner)
+    private static object CloneSet(Type setType, IEnumerable source, Func<object, object> elementCloner)
     {
-        var elementType = setType.GetGenericArguments()[0];
-        var hashSetType = typeof(HashSet<>).MakeGenericType(elementType);
-        var set = Activator.CreateInstance(hashSetType);
-        var addMethod = hashSetType.GetMethod("Add");
+        var elementType = setType.GetGenericArguments().FirstOrDefault() ??
+            throw new ArgumentException($"Set type {setType.Name} must have a generic argument");
 
-        foreach (var item in source)
+        var hashSetType = typeof(HashSet<>).MakeGenericType(elementType);
+        var set = Activator.CreateInstance(hashSetType) ??
+            throw new InvalidOperationException($"Failed to create set of type {hashSetType.Name}");
+
+        var addMethod = hashSetType.GetMethod("Add") ??
+            throw new InvalidOperationException($"Could not find Add method on set type {hashSetType.Name}");
+
+        try
         {
-            var clonedItem = elementCloner(item);
-            addMethod.Invoke(set, [clonedItem]);
+            foreach (var item in source)
+            {
+                var clonedItem = elementCloner(item);
+                addMethod.Invoke(set, [clonedItem]);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new ComparisonException($"Failed to clone set of type {setType.Name}", "", ex);
         }
 
         return set;
     }
 
-    private static object CloneQueueOrStack(Type collectionType, IEnumerable source,
-        Func<object, object> elementCloner)
+    private static object CloneQueueOrStack(Type collectionType, IEnumerable source, Func<object, object> elementCloner)
     {
-        var elementType = collectionType.GetGenericArguments()[0];
+        var elementType = collectionType.GetGenericArguments().FirstOrDefault() ??
+            throw new ArgumentException($"Collection type {collectionType.Name} must have a generic argument");
+
         var items = source.Cast<object>().Select(elementCloner).ToList();
+        var genericType = collectionType.GetGenericTypeDefinition();
 
-        if (collectionType.GetGenericTypeDefinition() == typeof(Queue<>))
+        if (genericType == typeof(Queue<>))
         {
-            var queueType = typeof(Queue<>).MakeGenericType(elementType);
-            var queue = Activator.CreateInstance(queueType);
-            var enqueueMethod = queueType.GetMethod("Enqueue");
+            return CloneQueue(elementType, items);
+        }
+        
+        if (genericType == typeof(Stack<>))
+        {
+            return CloneStack(elementType, items);
+        }
 
+        throw new ArgumentException($"Unsupported collection type: {collectionType.Name}");
+    }
+
+    private static object CloneQueue(Type elementType, IEnumerable<object> items)
+    {
+        var queueType = typeof(Queue<>).MakeGenericType(elementType);
+        var queue = Activator.CreateInstance(queueType) ??
+            throw new InvalidOperationException($"Failed to create queue of type {queueType.Name}");
+
+        var enqueueMethod = queueType.GetMethod("Enqueue") ??
+            throw new InvalidOperationException($"Could not find Enqueue method on queue type {queueType.Name}");
+
+        try
+        {
             foreach (var item in items)
             {
                 enqueueMethod.Invoke(queue, [item]);
             }
-
-            return queue;
         }
-        else // Stack<T>
+        catch (Exception ex)
         {
-            var stackType = typeof(Stack<>).MakeGenericType(elementType);
-            var stack = Activator.CreateInstance(stackType);
-            var pushMethod = stackType.GetMethod("Push");
+            throw new ComparisonException($"Failed to clone queue of type {queueType.Name}", "", ex);
+        }
 
-            // Push in reverse order to maintain original order
-            foreach (var item in items.AsEnumerable().Reverse())
+        return queue;
+    }
+
+    private static object CloneStack(Type elementType, IEnumerable<object> items)
+    {
+        var stackType = typeof(Stack<>).MakeGenericType(elementType);
+        var stack = Activator.CreateInstance(stackType) ??
+            throw new InvalidOperationException($"Failed to create stack of type {stackType.Name}");
+
+        var pushMethod = stackType.GetMethod("Push") ??
+            throw new InvalidOperationException($"Could not find Push method on stack type {stackType.Name}");
+
+        try
+        {
+            foreach (var item in items.Reverse())
             {
                 pushMethod.Invoke(stack, [item]);
             }
-
-            return stack;
         }
+        catch (Exception ex)
+        {
+            throw new ComparisonException($"Failed to clone stack of type {stackType.Name}", "", ex);
+        }
+
+        return stack;
     }
 
-    private static object CloneGenericList(Type collectionType, IEnumerable source,
-        Func<object, object> elementCloner)
+    private static object CloneGenericList(Type collectionType, IEnumerable source, Func<object, object> elementCloner)
     {
         var elementType = GetElementType(collectionType);
         var listType = typeof(List<>).MakeGenericType(elementType);
-        var list = Activator.CreateInstance(listType);
-        var addMethod = listType.GetMethod("Add");
+        var list = Activator.CreateInstance(listType) ??
+            throw new InvalidOperationException($"Failed to create list of type {listType.Name}");
 
-        foreach (var item in source)
-        {
-            var clonedItem = elementCloner(item);
-            addMethod.Invoke(list, [clonedItem]);
-        }
+        var addMethod = listType.GetMethod("Add") ??
+            throw new InvalidOperationException($"Could not find Add method on list type {listType.Name}");
 
-        // If the original type was a List<T>, return as is
-        if (IsGenericList(collectionType))
-        {
-            return list;
-        }
-
-        // Try to convert to the original collection type
         try
         {
+            foreach (var item in source)
+            {
+                var clonedItem = elementCloner(item);
+                addMethod.Invoke(list, [clonedItem]);
+            }
+
+            // If the original type was a List<T>, return as is
+            if (IsGenericList(collectionType))
+            {
+                return list;
+            }
+
+            // Try to convert to the original collection type
             var constructor = collectionType.GetConstructor(
                 [typeof(IEnumerable<>).MakeGenericType(elementType)]);
 
-            if (constructor != null)
-            {
-                return constructor.Invoke([list]);
-            }
+            return constructor?.Invoke([list]) ?? list;
         }
-        catch
+        catch (Exception ex)
         {
-            // Fall back to list if conversion fails
+            throw new ComparisonException($"Failed to clone list of type {collectionType.Name}", "", ex);
         }
-
-        return list;
     }
 
     private static bool IsDictionary(Type type)
     {
-        return type.IsGenericType &&
-               (type.GetGenericTypeDefinition() == typeof(Dictionary<,>) ||
-                type.GetGenericTypeDefinition() == typeof(IDictionary<,>));
+        return type.IsGenericType && (
+            type.GetGenericTypeDefinition() == typeof(Dictionary<,>) ||
+            type.GetGenericTypeDefinition() == typeof(IDictionary<,>));
     }
 
     private static bool IsSet(Type type)
     {
-        return type.IsGenericType &&
-               (type.GetGenericTypeDefinition() == typeof(HashSet<>) ||
-                type.GetGenericTypeDefinition() == typeof(ISet<>));
+        return type.IsGenericType && (
+            type.GetGenericTypeDefinition() == typeof(HashSet<>) ||
+            type.GetGenericTypeDefinition() == typeof(ISet<>));
     }
 
     private static bool IsQueueOrStack(Type type)
     {
-        return type.IsGenericType &&
-               (type.GetGenericTypeDefinition() == typeof(Queue<>) ||
-                type.GetGenericTypeDefinition() == typeof(Stack<>));
+        return type.IsGenericType && (
+            type.GetGenericTypeDefinition() == typeof(Queue<>) ||
+            type.GetGenericTypeDefinition() == typeof(Stack<>));
     }
 
     private static bool IsGenericList(Type type)
@@ -194,24 +247,27 @@ internal static class CollectionHandling
 
     private static Type GetElementType(Type collectionType)
     {
-        if (collectionType.IsArray)
+        return ElementTypeCache.GetOrAdd(collectionType, type =>
         {
-            return collectionType.GetElementType();
-        }
-
-        if (collectionType.IsGenericType)
-        {
-            var genericArgs = collectionType.GetGenericArguments();
-            if (genericArgs.Length == 1)
+            if (type.IsArray)
             {
-                return genericArgs[0];
+                return type.GetElementType() ?? typeof(object);
             }
-        }
 
-        var enumType = collectionType.GetInterfaces()
-            .FirstOrDefault(i => i.IsGenericType &&
-                                 i.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+            if (type.IsGenericType)
+            {
+                var genericArgs = type.GetGenericArguments();
+                if (genericArgs.Length == 1)
+                {
+                    return genericArgs[0];
+                }
+            }
 
-        return enumType?.GetGenericArguments()[0] ?? typeof(object);
+            var enumType = type.GetInterfaces()
+                .FirstOrDefault(i => i.IsGenericType &&
+                                   i.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+
+            return enumType?.GetGenericArguments()[0] ?? typeof(object);
+        });
     }
 }
--- ObjectComparison/NumericComparison.cs
+++ ObjectComparison/NumericComparison.cs
@@ -5,41 +5,50 @@
 /// </summary>
 internal static class NumericComparison
 {
+    private const double DefaultEpsilon = 1e-10;
+
     public static bool AreFloatingPointEqual(double value1, double value2, ComparisonConfig config)
     {
+        ArgumentNullException.ThrowIfNull(config);
+
         if (double.IsNaN(value1) && double.IsNaN(value2))
             return true;
 
         if (double.IsInfinity(value1) || double.IsInfinity(value2))
-            return value1 == value2;
+            return value1.Equals(value2);
 
-        if (config.UseRelativeFloatingPointComparison)
-        {
-            return AreRelativelyEqual(value1, value2, config.FloatingPointTolerance);
-        }
-
-        return Math.Abs(value1 - value2) <= config.FloatingPointTolerance;
+        return config.UseRelativeFloatingPointComparison
+            ? AreRelativelyEqual(value1, value2, config.FloatingPointTolerance)
+            : Math.Abs(value1 - value2) <= config.FloatingPointTolerance;
     }
 
     public static bool AreFloatingPointEqual(float value1, float value2, ComparisonConfig config)
     {
+        ArgumentNullException.ThrowIfNull(config);
+
         if (float.IsNaN(value1) && float.IsNaN(value2))
             return true;
 
         if (float.IsInfinity(value1) || float.IsInfinity(value2))
-            return value1 == value2;
+            return value1.Equals(value2);
 
-        if (config.UseRelativeFloatingPointComparison)
-        {
-            return AreRelativelyEqual(value1, value2, (float)config.FloatingPointTolerance);
-        }
+        return config.UseRelativeFloatingPointComparison
+            ? AreRelativelyEqual(value1, value2, (float)config.FloatingPointTolerance)
+            : Math.Abs(value1 - value2) <= config.FloatingPointTolerance;
+    }
 
-        return Math.Abs(value1 - value2) <= config.FloatingPointTolerance;
+    public static bool AreDecimalsEqual(decimal value1, decimal value2, ComparisonConfig config)
+    {
+        ArgumentNullException.ThrowIfNull(config);
+        
+        var rounded1 = decimal.Round(value1, config.DecimalPrecision);
+        var rounded2 = decimal.Round(value2, config.DecimalPrecision);
+        return rounded1 == rounded2;
     }
 
     private static bool AreRelativelyEqual(double value1, double value2, double relativeTolerance)
     {
-        if (value1 == value2)
+        if (value1.Equals(value2))
             return true;
 
         var absoluteDifference = Math.Abs(value1 - value2);
@@ -53,7 +62,7 @@ internal static class NumericComparison
 
     private static bool AreRelativelyEqual(float value1, float value2, float relativeTolerance)
     {
-        if (value1 == value2)
+        if (value1.Equals(value2))
             return true;
 
         var absoluteDifference = Math.Abs(value1 - value2);
